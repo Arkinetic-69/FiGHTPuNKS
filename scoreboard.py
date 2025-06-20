1ba304b552cb603f0b2f0b15f738caa4fe06c15a
dubs <159505808+dubsuk1@users.noreply.github.com>
2025-06-20 19:26:41 +0800

    added rm.py and scoreboard.py files

---
+++ scoreboard.py
@@ -0,0 +1,20 @@
+import pygame.font
+
+class Scoreboard:
+    """Class to report scoring information."""
+    
+    def __init__ (self, settings):
+        """Initializes scorekeeping attributes."""
+        self.settings = settings
+        self.screen = settings.screen
+        self.screen_rect = self.screen.get_rect()
+        
+        # Font settings for scoring information
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Initialize the score
+        self.score = 0
+
+        # Prepare the initial score image
+        self.prep_score()
